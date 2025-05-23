c1be6460f66e29b33dfb7d95b7398895465c13ff
xiaoyingliu <xiaoyingliu@google.com>
2025-05-23 20:26:37 +0000

    Support 'adb shell dumpsys wifi PasspointManager' command
    This command dumps only important parameters of the passpoints.
    
    Bug: 419613507
    Test: manual test with the adb command
    Flag: EXEMPT bugfix
    Change-Id: I1847e221096762e52503c279f53357f96db0b03e

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -6038,6 +6038,8 @@ public class WifiServiceImpl extends IWifiManager.Stub {
                 mWifiNetworkSuggestionsManager.dump(fd, pw, args);
             } else if (WifiCarrierInfoManager.DUMP_ARG.equals(arg0)) {
                 mWifiCarrierInfoManager.dump(fd, pw, args);
+            } else if (PasspointManager.DUMP_ARG.equals(arg0)) {
+                mPasspointManager.dump(pw, true);
             } else {
                 pw.println("Verbose logging is " + (mVerboseLoggingEnabled ? "on" : "off"));
                 pw.println("mVerboseLoggingLevel " + mVerboseLoggingLevel);
@@ -6092,7 +6094,7 @@ public class WifiServiceImpl extends IWifiManager.Stub {
                 pw.println();
                 pw.println("WifiApConfigStore config: " + mWifiApConfigStore.getApConfiguration());
                 pw.println();
-                mPasspointManager.dump(pw);
+                mPasspointManager.dump(pw, false);
                 mWifiInjector.getPasspointNetworkNominateHelper().dump(pw);
                 pw.println();
                 mWifiInjector.getWifiDiagnostics().captureBugReportData(
